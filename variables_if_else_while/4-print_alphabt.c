051aecd4632ea761f718e94d729c7de94b863367
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-09 13:27:53 +0100

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,10 @@ int main(void)
 	char i;
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		if (i == 'e'; i == 'q')
-			break;
+		if (i != 'e' && i != 'q')
+			putchar(i);
 	}
-	putchar(i);
+
 	putchar('\n');
 	return (0);
 }
