92b5869b9a8a268563eef38dd9fb2a88b46e6d3b
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-10 15:47:56 +0100

    4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,10 +2,11 @@
 
 /**
  * _isalpha -checks for alphabetic character,
- * @c: the character to check
+ * @c: the character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
 int _isalpha(int c);
 {
+
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
