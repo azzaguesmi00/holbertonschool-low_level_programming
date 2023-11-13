fa73d8ac2523bae1ae3843d228065ab2c6c13567
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-13 21:20:12 +0100

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- * print_most_numbers- print numbers 0 to 9
- * expect 2 and 4
- *
+ *print_most_numbers - prints numbers, from 0 to 9 expect 2 and 4
  *Return: void
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	char c;
 
