517e0bb1d5cc9701a17d7a13daa52d06e77e674c
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-13 20:18:22 +0100

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include <main.h>
 
 /**
- * isupper- check for uppercase character
+ * _isupper- check for uppercase character
  * 0c: the character to be checked
  *
  * Return: Return: 1 if c is a letter, 0 otherwise
