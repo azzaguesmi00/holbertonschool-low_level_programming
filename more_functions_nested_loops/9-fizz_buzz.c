eeb992a501fbdf75ddaa309a0918fad0d32b51ea
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-14 15:11:41 +0100

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 	for (i = 0; i < 101; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("fizzbuzz");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
