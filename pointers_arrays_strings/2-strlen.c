6d7416b0749834273eab807f4849110c8891ac5b
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-15 16:09:42 +0100

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strlen :returns the length of a string
+ * _strlen :returns the length of a string.
  * @s: The string to get the length of.
  *
  * Return: The length of @s
