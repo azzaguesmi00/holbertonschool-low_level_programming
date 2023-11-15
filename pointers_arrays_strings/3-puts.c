dacbe7ca0c05fe7bb5ec4793fc1fc2901211e982
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-15 17:06:48 +0100

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- *_puts - prints a string
- * @str: string to print
+ * _puts - Prints a string
+ * @str: The string to print
  *
- * On success: return the number of characters printed
+ * Return: void
  */
-
 void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
-		_putchar((*(str++)));
+		_putchar(*str);
 	}
 
-	_putchar('\n');
+	_putchar('\n')
 }
