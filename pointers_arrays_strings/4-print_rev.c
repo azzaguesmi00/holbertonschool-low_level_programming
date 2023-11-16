6c72b09d0d28f9873c4ac5cc3572b4819b8f5e5a
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-16 11:10:19 +0100

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,14 @@ void print_rev(char *s)
 	int len;
 	int i;
 
-	if(*s == '\0')
+	if (*s == '\0')
 		_putchar('\n');
-	else{
-		i =0;
+	else
+	{
+		i = 0;
 		len = strlen(s);
-		while (i < len){
+		while (i < len)
+		{
 			_putchar(s[len - i - 1]);
 			i++;
 		}
