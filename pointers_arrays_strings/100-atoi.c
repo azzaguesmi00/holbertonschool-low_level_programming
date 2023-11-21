c113ac68dbf57a251354f0c7689cca64c7363008
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-21 22:54:03 +0100

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * int _atoi - convert a string to an integer
+ * @s: the string to b converted
+ * Return :integer value of the converted string
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int total = 0;
+
+	do
+	{
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			total = (total * 10) + (*s - '0');
+
+		else if (total > 0)
+			break;
+
+	} while (*s++);
+
+	return (total * sign);
+}
