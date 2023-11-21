23964251be29a83fc7d523a3faf2758ffb483cbf
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-21 23:49:27 +0100

    0-strcat.c

---
+++ 2pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  _strcat -concatenates two strings.
+ * @dest: pointer to the string to be concatenated
+ * @src: string to be appended to @dest.
+ *
+ * Return: A pointer to the destination string
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	
+}
