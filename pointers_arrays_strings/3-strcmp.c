61d6686d6caffa4fd60bdbac76d7affdb4498f44
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-22 15:40:46 +0100

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,18 +8,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	while (*s1 != '\0' && *s2 != '\0')
+	int i = 0, c = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0' && c == 0)
 	{
-		if (*s1 != *s2)
-		{
-			i = 1;
-		}
-		s1++;
-		s2++;
+		c = s1[i] - s2[i];
+		i++;
 	}
-	if (i == 0)
-		return 0;
-	else
-		return 1;
+	return (c);
 }
