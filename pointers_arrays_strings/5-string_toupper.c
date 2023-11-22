fa622127a00f1ccad4b616a64526465b58ee2276
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-22 13:41:13 +0100

    string toupper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -9,12 +9,10 @@
 char *string_toupper(char *)
 {
     int i;
-
-    for (i = 0; *str != '\0'; i++)
+    for (i = 0; str[i] != '\0'; i++)
     {
         if (str[i] >= 'a' && str[i] <= 'z')
-            str[i] -= 32;
+            str[i] = (int)str[i] - 32;
     }
     return (str);
 }
-
