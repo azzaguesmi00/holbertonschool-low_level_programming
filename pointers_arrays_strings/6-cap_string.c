9838c3cba00673aea10c98fa316645649a3bb735
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-22 15:57:02 +0100

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string -capitalizes all words of a string.
+ * @s: string
+ *Return : 0
+ */
+char *cap_string(char *s)
+{
+	int i, cap, l = 0;
+
+	while (*(s + l))
+		l++;
+	if (s[0] <= 'z' && s[0] >= 'a')
+		s[0] -= 32;
+	for (i = 0; i < l; i++)
+	{
+		if (s[i] == '\t' || s[i] == '\n')
+			cap = 1;
+		else if (s[i] == ' ' || s[i] == '.')
+			cap = 1;
+		else if (s[i] == ',' || s[i] == ';')
+			cap = 1;
+		else if (s[i] == '!' || s[i] == '?')
+			cap = 1;
+		else if (s[i] == '(' || s[i] == ')')
+			cap = 1;
+		else if (s[i] == '{' || s[i] == '}')
+			cap = 1;
+		else if (s[i] <= 'z' && s[i] >= 'a' && cap != 0)
+		{
+			s[i] -= 32;
+			cap = 0;
+		}
+		else
+			cap = 0;
+	}
+	return (s);
+}
