81c37ac81b370c42e8bbaa6fe1e6f741a204465b
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-25 14:33:48 +0100

    strchar

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr-locates a character in a string.
+ * @s: string 
+ * @c:character to locate
+ * Return : 0
+ */
+char *_strchr(char *s, char c)
+{
+	int i, len;
+	len = strlen;
+
+	while (i < len + 1)
+	{
+		if (s[i] == C)
+		{
+			s = s + 1 break;
+		}
+		i++;
+	}
+	if (*s = !c)
+		return (NULL);
+	return (s);
+}
