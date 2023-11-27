d27d5e40453a256fe2f367e038f8953f5e5fed34
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-27 21:11:24 +0100

    powrec

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 /**
  * _pow_recursion-returns the value of x raised to the power of y.
- * @x:
- * @y:
- * Return:
+ * @x: integer
+ * @y: integer
+ * Return: 0
  */
 int _pow_recursion(int x, int y)
 {
