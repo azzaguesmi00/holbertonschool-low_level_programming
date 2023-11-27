4068d85c234b39fe0d5cfdb7ce3eb710eabfa9cb
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-27 13:44:21 +0100

    strlenrecursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -9,12 +9,10 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
 	if (*s != '\0')
 	{
-		return(  _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s++));
 	}
-	return (i);
+	else
+		return (1);
 }
-
