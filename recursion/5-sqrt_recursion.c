572d78373060fc6d79b9c579dbddfd42e601ab5b
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-27 21:28:47 +0100

    5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - chech the code.
+ * @n: int
+ * @i: int
+ * Return: Always 0.
+ */
+int sqr(int n, int i);
+int _sqrt_recursion(int n)
+{
+	return (sqr(n, 1));
+}
+/**
+ * sqr - check the code.
+ * @n: int
+ * @i: int
+ * Return: Always 0.
+ */
+
+int sqr(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i < n)
+	{
+		return (sqr(n, i + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
