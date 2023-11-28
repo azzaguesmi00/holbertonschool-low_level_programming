1d458c965e29d013b1850057872636d8c50ad871
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-28 22:17:27 +0100

    task2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -10,15 +10,9 @@ int main(int argc, char **argv[])
 {
 	int i;
 
-	printf("%d\n", argc - 1);
-
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s/n", argv[i]);
-	}
-	if (i < argc - 1)
-	{
-		printf("\n");
 	}
 
 	return (0);
