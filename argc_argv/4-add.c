f3ce3829836cb214683156b5c2cb967d079a443c
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-28 22:51:08 +0100

    task4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+ *main - print positive numbers
+ *@argc: int
+ *@argv: pointer to an array
+ *Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int n = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		n = n + (atoi(argv[argc]));
+	}
+	printf("%d\n", n);
+	return (0);
+}
