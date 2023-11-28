04543e3ab241a8eef51e14d4c11e8ded39a43b18
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-28 22:42:08 +0100

    task3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,17 +9,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int mul;
-
 	if (argc != 3)
 	{
-		mul = atoi(argv[1]) * atoi (argv[2]);
-		printf("%d\n", mul);
-	}
-	else
-	{
-		printf ("Error\n");
+		printf("Error\n");
 		return (1);
 	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
