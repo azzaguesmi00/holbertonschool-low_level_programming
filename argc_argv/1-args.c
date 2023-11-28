a347507847597358a72b4687c7779f65ca298881
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-28 21:47:10 +0100

    task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -check the code
+ * @argv: char
+ * @argc: int
+ * Return : 0
+ */
+int main(int argc, char *argv[])
+
+{
+
+	printf("Number of arguments: %d\n", argc - 1);
+
+	return (0);
+}
+
