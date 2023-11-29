5fbd76c0503352e57b3827823f44491f8442b520
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-11-29 19:19:26 +0100

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG
+#define DOG
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- check the code
+ * struct dog- description of the dog
+ * @name: name of the dog
+ * @age: old of the dog
+ * @owner: the person that owns the dog
+ * Return: 0
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
