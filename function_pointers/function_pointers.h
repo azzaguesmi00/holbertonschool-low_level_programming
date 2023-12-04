4762a0a24e6cd222f0a25c0b3f5ac06d6b900411
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-12-04 10:57:46 +0100

    main

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,4 @@
-#ifndef
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
@@ -8,4 +8,3 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-#endif
