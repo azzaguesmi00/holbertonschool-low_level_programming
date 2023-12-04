92fe249fe5bf48ebd649623bb094fce519298c99
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-12-04 16:33:38 +0100

    int

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,14 +12,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 && array == NULL && cmp == NULL)
-	{
+	if (size > 0 && array != NULL && cmp != NULL)
 		for (i = 0; i < size; i++)
-
-			if (*cmp(array[i]) == 1)
-			{
+			if ((*cmp)(array[i]) == 1)
 				return (i);
-			}
-	}
 	return (-1);
 }
