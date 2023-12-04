109c3cf6cba672c46b2126589fa11841e656f56d
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-12-04 16:17:20 +0100

    main

---
+++ function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+/**
+ * main- check the code
+ * @argc: int
+ * @argv: char
+ * Return : 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((*get_op_func(argv[2])) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", (*get_op_func(argv[2]))(a, b));
+
+	return (0);
+}
