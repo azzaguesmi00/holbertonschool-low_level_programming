fb59f7913f4d673f9268173e09fa172b493011c7
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-12-05 20:45:09 +0100

    main

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,20 @@
 #ifndef VARIADIC_FUNC
 #define VARIADIC_FUNC
 
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct om
+{
+	char op;
+	void (*take)(va_list *);
+} om_t;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
