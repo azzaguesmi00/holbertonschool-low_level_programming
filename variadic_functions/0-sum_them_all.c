38817986fa828499517f1a9a1553ea271c970ca0
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-12-05 16:50:24 +0100

    task0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all : gives the sum of all operators
+ * @n: unsigned int
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, x = 0;
+
+	va_list ptr;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+		x += va_arg(ptr, int);
+
+	va_end(ptr);
+	
+	return (x);
+}
