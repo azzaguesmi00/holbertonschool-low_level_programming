feb357c1330ab71caeeac90f15b1853d51d2db20
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-12-10 21:51:11 +0100

    task4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * free_grid - check the code
+ * @grid: int 
+ * @height: height 
+ * Return: 0 or null
+ */
+void free_grid(int **grid, int height)
+{
+    int x;
+
+    for (x = height - 1; x >= 0; --x)
+        free(grid[x]);
+    free(grid);
+}
