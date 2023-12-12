00715d3f3f8f3f2695d0bc13301ee2aee0e70c22
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-12-12 22:28:25 +0100

    task2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,27 +12,17 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	if (head == NULL || str == NULL)
+	do
 	{
-		return (NULL);
-	}
-
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+		new_node = malloc(sizeof(list_t));
+	} while (new_node == NULL);
 
 	new_node->len = strlen(str);
-	new_node->next = *head; // Set the next pointer to the current head
-	*head = new_node;		// Update the head to point to the new node
+	new_node->str = strdup(str);
 
-	return (new_node);
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (*head);
 }
