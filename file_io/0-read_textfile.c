d9395cee7664491dbb0d52f4e7917d319bf9ad2c
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-12-20 14:59:05 +0100

    task0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * read_textfile - check the code
+ * @filename: const char *
+ * @letters: size_t
+ * Return: number of bytes or 0 if error
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	ssize_t bytes;
+	char buffer[1024 * 69];
+
+	if (!filename || !letters)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+
+	bytes = read(file, buffer, letters);
+	bytes = write(STDOUT_FILENO, buffer, bytes);
+	close(file);
+
+	return (bytes);
+}
